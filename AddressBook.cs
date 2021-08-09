86b576e6e5db6f30648617ffe86123cad8f0ef39
vk0808 <vk4765712@gmail.com>
2021-08-09 23:09:28 +0530

    fix: refactor code

--- AddressBook.cs
+++ AddressBook.cs
@@ -4,8 +4,59 @@ using System.Text;
 
 namespace AddressBook
 {
+    public class Persons
+    {
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+        public string City
+        {
+            get;
+            set;
+        }
+        public string State
+        {
+            get;
+            set;
+        }
+        public string Zip
+        {
+            get;
+            set;
+        }
+    }
+
     public class AddressBook
     {
+        List<Persons> People;
+
+        public AddressBook()
+        {
+            People = new List<Persons>();
+        }
+
         public string welcome()
         {
             return "Welcome to Address Book Program";
--- Person.cs
+++ Person.cs
@@ -8,23 +8,23 @@ namespace AddressBook
     {
         public string firstName;
         public string lastName;
+        public string phoneNumber;
+        public string email;
         public string address;
         public string city;
         public string state;
         public string zip;
-        public string phoneNumber;
-        public string email;
 
         public Person(string firstNname, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
             this.firstName = firstNname;
             this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
             this.address = address;
             this.city = city;
             this.state = state;
             this.zip = zip;
-            this.phoneNumber = phoneNumber;
-            this.email = email;
         }
     }
 }
